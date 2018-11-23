b0597df50e613920c8874e359292533b4f5c8650
Matthew Greenhalgh <MGreenhalgh5@uclan.ac.uk>
2018-11-23 13:50:32 +0000

    Created a loop for the scroll magic fade

--- landing-page.js
+++ landing-page.js
@@ -4,30 +4,17 @@ $(document).ready(function()
     //init ScrollMagic
 var controller = new ScrollMagic.Controller();
 
-    //build a scene
-    var ourScene = new ScrollMagic.Scene({
-        triggerElement: '#product01'
 
-    })
-        .setClassToggle('#product01', 'fade-in') //add class to product01
-        .addTo(controller);
 
-    ourScene = new ScrollMagic.Scene({
-        triggerElement: '#product02'
-    })
-        .setClassToggle('#product02', 'fade-in') //add class to product01
-        .addTo(controller);
+    //Loop through each product
+    $('.product').each(function () {
+        //build a scene
+        var ourScene = new ScrollMagic.Scene({
+            triggerElement: '#product01'
 
-    ourScene = new ScrollMagic.Scene({
-        triggerElement: '#product03'
-    })
-        .setClassToggle('#product03', 'fade-in') //add class to product01
-        .addTo(controller);
-
-    ourScene = new ScrollMagic.Scene({
-        triggerElement: '#product04'
-    })
-        .setClassToggle('#product04', 'fade-in') //add class to product01
-        .addTo(controller);
+        })
+            .setClassToggle('#product01', 'fade-in') //add class to product01
+            .addTo(controller);
+    });
 
 });
