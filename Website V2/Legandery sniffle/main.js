7c6db8f2086c40ec30026bcd8bf23e5ca40795a9
Matthew Greenhalgh <MGreenhalgh5@uclan.ac.uk>
2018-11-30 15:39:41 +0000

    Website V2

---
+++ Website V2/Legandery sniffle/main.js
@@ -0,0 +1,67 @@
+$(document).ready(function()
+{
+
+    //init ScrollMagic
+    var controller = new ScrollMagic.Controller();
+
+
+    //Loop through each product
+    $('.product').each(function () {
+        //build a scene
+        var ourScene = new ScrollMagic.Scene({
+            triggerElement: this,
+            duration: '90%'
+        })
+            .setClassToggle(this, 'fade-in') //add class to product01
+            .addTo(controller);
+    });
+
+    var pinLandingPage = new ScrollMagic.Scene({
+        triggerElement: '#intro',
+        triggerHook: 1,
+        duration: '100%'
+    })
+        .setPin('#lintro',{ pushFollowers: false})
+        .addTo(controller);
+
+    //pin again
+    var pinLandingPage2 = new ScrollMagic.Scene({
+        triggerElement: '#product01',
+        triggerHook: 0.9
+    })
+        .setPin('#intro',{ pushFollowers: false})
+        .addTo(controller);
+
+});
+
+
+// ScrollSpy
+
+function scrollNav() {
+    $('.nav-item a').click(function () {
+        //Toggle Class
+        $(".active").removeClass("active");
+        $(this).closest('li').addClass("active");
+        var theClass = $(this).attr("class");
+        $('.' + theClass).parent('li').addClass('active');
+        //Animate
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 160
+        }, 200);
+        return false;
+    });
+    $('.scrollTop a').scrollTop();
+}
+scrollNav();
+
+
+
+
+
+
+
+
+
+
+
+
